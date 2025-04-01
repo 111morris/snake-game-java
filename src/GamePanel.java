b4b7101f09c69dbc11577cdb53051ce0318780af
mulandi <mulandimorris1@gmail.com>
2025-04-01 22:32:51 +0300

    adding components

---
+++ src/GamePanel.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.random.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+  GamePanel(){
+
+  }
+  public void startGame(){
+
+  }
+  public void paintComponent(Graphics g){
+
+  }
+  public void draw(){
+
+  }
+  public void move() {
+
+  }
+  public void checApple(){
+
+  }
+  public void checkCollisions(){
+
+  }
+  public void gameOver(ActionEvent e){
+
+  }
+  @Override
+  public void actionPerformed(ActionEvent actionEvent) {
+
+  }
+
+  public class MyKeyAdapter extends KeyAdapter{
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+  }
+}
